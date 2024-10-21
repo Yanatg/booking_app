c0a4b1f8d397a51b91978e58727028b9fba82a24
Yanatchara JERAJA <yanatchara.j@ku.th>
2024-10-21 20:06:31 +0700

    login setup

---
+++ app/accommodation/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { useSession } from "next-auth/react";
+
+export default function Home() {
+  const { data: session } = useSession();
+  console.log(session);
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
