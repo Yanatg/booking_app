9451a7b8f55cd0cfca64ecd61c0dfbd6c3c1da2d
Yanatchara JERAJA <yanatchara.j@ku.th>
2024-10-21 21:33:37 +0700

    logout

--- app/accommodation/page.tsx
+++ app/accommodation/page.tsx
@@ -1,12 +1,23 @@
 "use client";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 
 export default function Home() {
   const { data: session } = useSession();
   console.log(session);
+
+  const handleLogout = () => {
+    signOut();
+  }
+
+  if (!session) {
+    redirect("/");
+  }
+
   return (
     <div>
       <h1>Home</h1>
+      <div className="w-[100px] h-[100px] bg-red-500" onClick={handleLogout}>logout</div>
     </div>
   );
 }
